bf36e46ed2020206e6e268b1744efd2f1e7464a1
mandyraff <mraffert@andrew.cmu.edu>
2025-08-15 13:32:47 -0400

    feat: add site footer and navbar styles, implement global styles, and create layout structure
    - Added `_site-footer.scss` for footer styling.
    - Added `_site-navbar.scss` for navbar styling.
    - Created `style.scss` to import necessary styles and define color variables.
    - Implemented `Layout.astro` for consistent page structure.
    - Introduced `utils.ts` for utility functions.
    - Created `aboutme.astro` page with basic content.
    - Updated `index.astro` to include layout and navigation links.
    - Added `global.css` for Tailwind CSS and custom styles.
    - Updated `tsconfig.json` to include path aliases for cleaner imports.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mandyrafferty.github.io',
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
